26ab8a66d9d7ad7c8e7ca4d215d1c9b30372612f
Nnamani Ugochukwu F <royal.ugoh@gmail.com>
2020-06-02 12:56:58 +0100

    optimized Game.js using map

--- src/Game.js
+++ src/Game.js
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Game.css';
 // STAR MATCH - Starting Template
 
 const Game = () => {
+
+	const [stars, setStars] = useState(utils.random(1, 9));
   return (
     <div className="game">
       <div className="help">
@@ -10,26 +12,18 @@ const Game = () => {
       </div>
       <div className="body">
         <div className="left">
-          <div className="star" />
-          <div className="star" />
-          <div className="star" />
-          <div className="star" />
-          <div className="star" />
-          <div className="star" />
-          <div className="star" />
-          <div className="star" />
-          <div className="star" />
+          {
+			  utils.range(1, stars).map(starId => {
+				return <div key={starId} className="star"></div>
+			  })
+		  }
         </div>
         <div className="right">
-          <button className="number">1</button>
-          <button className="number">2</button>
-          <button className="number">3</button>
-          <button className="number">4</button>
-          <button className="number">5</button>
-          <button className="number">6</button>
-          <button className="number">7</button>
-          <button className="number">8</button>
-          <button className="number">9</button>
+          {
+			utils.range(1, 9).map(number => {
+				return <button key={number} className="number">{number}</button>
+			})
+		  }
         </div>
       </div>
       <div className="timer">Time Remaining: 10</div>
